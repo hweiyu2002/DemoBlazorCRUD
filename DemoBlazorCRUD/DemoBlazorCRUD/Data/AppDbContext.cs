958966117ed81910735c074ab489da7a5724d7f7
hweiyu2002 <hweiyu1001@gmail.com>
2024-03-25 23:06:05 +0800

    加入專案檔案。

---
+++ DemoBlazorCRUD/DemoBlazorCRUD/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SharedLibrary.Models;
+
+namespace DemoBlazorCRUD.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { 
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
